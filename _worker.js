5c2073b324b6a9521a5110afe1f573d6534b1b76
djyx <70806603+djyx@users.noreply.github.com>
2024-07-19 13:43:30 +0000

    _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'a6c11f49-2a7f-48c4-a580-bf23623b0524';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
